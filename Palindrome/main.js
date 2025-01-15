1a587e71c2d39844539bc2acfd357a89e9dac3b0
giuseppezaccato <giuseppezaccato@gmail.com>
2025-01-15 18:41:25 +0100

    ADD: Palindrome method*2

--- Palindrome/main.js
+++ Palindrome/main.js
@@ -4,42 +4,50 @@
 
 
 //* 1.1 concatenazione di metodi (methods.concat)
-let parola = prompt("Scrivi qui una parola (non preoccuparti delle maiuscole)", "iTopiNonAvevanoNipoti");
-// let Pali = "";
+let parola = prompt("Scrivi qui una parola (non preoccuparti delle maiuscole) ", "iTopiNonAvevanoNipoti");
+// ? let parolaInversa = revString(parola); 
+// non serve in realtà, anzi basta invocare la funzione al suo posto per accorgersene effettivamente
 
-function revString() {
-    parola.split("").reverse().join("").trim().toLowerCase();
+function revString(parola) {
+    return parola.split("").reverse().join("").trim(); //.toLowerCase();
     //? ragionamento concatenazione=> 
     //? creoArray.split("vuoto")-invertoOrdine.reverse()-tornoStringa.join("vuoto")-taglioEsterni.trim()-riduco.toLowerCase()
-
-    //! [.trim() e .toLowerCase()]=> FUNZIONANO SOLO E SOLO SE DOPO AVER PROCESSATO LA CONCATENAZIONE DI METODI
-    //! GLI DO "RETURN PAROLA" 
-    //! quindi return deve essere la conseguenza FINALE della funzione SEMPRE
-    return parola;
 }
+console.log(revString(parola));
+console.log(parola)
 
-if (revString(parola) == parola) {
+if (ToLow(parola) === ToLow(revString(parola))) {
     console.log(`la parola ${parola} è un palindromo`)
 } else {
     console.log(`la parola ${parola} NON è un palindromo`)
 }
 
 //* 1.2 metodo con ciclo FOR
-let parola1 = prompt("scrivimi qui una parola e ti dirò se è palindroma(non preoccuparti delle maiuscole)", "osso")
-let Pali1 = "";
+let parola1 = prompt("scrivimi qui una parola e ti dirò se è palindroma(non preoccuparti delle maiuscole)", "SaippuaKivikauppias")
+let parolaInversa1 = "";
 
 function revString1() {
     for (let i = parola1.length - 1; i >= 0; i--) {
-        Pali1 += parola1.charAt(i);
+        parolaInversa1 += parola1.charAt(i);
     }
-    return Pali1
+    return parolaInversa1;
+}
+console.log(ToLow(revString1()));
+//? qui devo invocare revstring1() per forza, perchè se richiamo parolaInversa1 avrò ""...PERCHè?!!!
+console.log(ToLow(parola1))
+
+function ToLow() {
+    return parola.toLowerCase();
 }
 
-if (revString1(parola1) == Pali1) {
+//? ToLow(parolaInversa1) qui funziona, nel console.log di riga 35 ho dovuto invocare (revString1()) PERCHè?!
+if (ToLow(parola1) === ToLow(parolaInversa1)) {
     console.log(`la parola ${parola1} è un palindromo`)
 } else {
     console.log(`la parola ${parola1} NON è un palindromo`)
 }
+
+//? righe 36 e 43 nonostante apparentemente siano opposti, mi danno l'esatto risultato atteso! COME MAI?
 
 
 
